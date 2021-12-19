d088fb1a30a2a981c8bca365cd453799b726b4e9
Kamran <56407135+k5924@users.noreply.github.com>
2021-12-19 17:17:48 +0000

    Refactored App Icon

---
+++ lib/src/widgets/app_icon.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppIcon extends StatelessWidget {
+
+  const AppIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Icon(
+      Icons.account_balance,
+      size: 214,
+    );
+  }
+}
--- lib/src/widgets/export.dart
+++ lib/src/widgets/export.dart
@@ -1,1 +1,2 @@
 // this is the export file for the widgets folder
+export 'app_icon.dart';
