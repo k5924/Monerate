c1b4de78e147b001a753ba77808da540917e8579
Kamran <56407135+k5924@users.noreply.github.com>
2022-02-01 17:37:43 +0000

    Implemented HP-110

---
+++ lib/src/screens/end_user_dashboard/news_article_screen.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class NewsArticleScreen extends StatefulWidget {
+  final String url;
+  const NewsArticleScreen({Key? key, required this.url}) : super(key: key);
+
+  @override
+  _NewsArticleScreenState createState() => _NewsArticleScreenState();
+}
+
+class _NewsArticleScreenState extends State<NewsArticleScreen> {
+  final Completer<WebViewController> _completer =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = AndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: WebView(
+          onWebViewCreated: (WebViewController webViewController) {
+            _completer.complete(webViewController);
+          },
+          initialUrl: widget.url,
+        ),
+      ),
+    );
+  }
+}
