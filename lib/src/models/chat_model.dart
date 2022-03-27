3dc64dbd65c641ac902fefc34a929826ae4fbc2e
Kamran <56407135+k5924@users.noreply.github.com>
2022-03-27 20:28:43 +0100

    Implemented HP-153

---
+++ lib/src/models/chat_model.dart
@@ -0,0 +1,42 @@
+import 'package:monerate/src/models/export.dart';
+
+class ChatModel {
+  String userID;
+  String firstName;
+  String lastName;
+  String chatType;
+  DateTime latestMessage;
+  List<MessageModel> messages;
+
+  ChatModel({
+    required this.userID,
+    required this.firstName,
+    required this.lastName,
+    required this.chatType,
+    required this.latestMessage,
+    required this.messages,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userID': userID,
+      'firstName': firstName,
+      'lastName': lastName,
+      'chatType': chatType,
+      'latestMessage': latestMessage.toUtc(),
+      'messages': messages,
+    };
+  }
+
+  @override
+  factory ChatModel.fromMap(Map<String, dynamic> map) {
+    return ChatModel(
+      userID: map['userID'] as String,
+      chatType: map['chatType'] as String,
+      latestMessage: map['latestMessage'] as DateTime,
+      messages: map['messages'] as List<MessageModel>,
+      firstName: map['firstName'] as String,
+      lastName: map['lastName'] as String,
+    );
+  }
+}
---
+++ test/src/models/chat_model_test.dart
@@ -0,0 +1,63 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:monerate/src/models/export.dart';
+
+void main() {
+  group('Chat Model:', () {
+    final DateTime time = DateTime(2000);
+    final MessageModel message = MessageModel(
+      userID: 'userID',
+      firstName: 'firstName',
+      lastName: 'lastName',
+      message: 'message',
+      createdAt: time,
+    );
+
+    test('Populated ChatModel.toMap() returns supplied values for each field',
+        () {
+      final ChatModel chatModel = ChatModel(
+        userID: 'userID',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        chatType: 'chatType',
+        latestMessage: time,
+        messages: [message],
+      );
+
+      expect(
+        chatModel.toMap(),
+        {
+          'userID': 'userID',
+          'firstName': 'firstName',
+          'lastName': 'lastName',
+          'chatType': 'chatType',
+          'latestMessage': time.toUtc(),
+          'messages': [message]
+        },
+      );
+    });
+
+    test('ChatModel.fromMap(map) returns map values in ChatModel format', () {
+      final chatModel = ChatModel.fromMap(
+        {
+          'userID': 'userID',
+          'firstName': 'firstName',
+          'lastName': 'lastName',
+          'chatType': 'chatType',
+          'latestMessage': time,
+          'messages': [message]
+        },
+      );
+      expect(
+        chatModel.toMap(),
+        {
+          'userID': 'userID',
+          'firstName': 'firstName',
+          'lastName': 'lastName',
+          'chatType': 'chatType',
+          'latestMessage': time.toUtc(),
+          'messages': [message]
+        },
+      );
+    });
+  });
+}
