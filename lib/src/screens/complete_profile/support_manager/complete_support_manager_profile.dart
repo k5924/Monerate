4799aedc53dbc7fa045373d76693f9684f0b866f
Kamran <56407135+k5924@users.noreply.github.com>
2022-04-09 17:55:17 +0100

    Fix issue when support signs up

--- lib/src/screens/complete_profile/support_manager/complete_support_manager_profile.dart
+++ lib/src/screens/complete_profile/support_manager/complete_support_manager_profile.dart
@@ -3,10 +3,12 @@
 import 'package:flutter/material.dart';
 import 'package:monerate/src/screens/export.dart';
 import 'package:monerate/src/widgets/export.dart';
+
 /// This page will be displayed when a user chooses their user type as support manager
 class CompleteSupportManagerProfile extends StatefulWidget {
   /// This variable stores the named route for this screen
   static const String kID = 'complete_support_manager_profile';
+
   /// This is the constructor for this screen
   const CompleteSupportManagerProfile({Key? key}) : super(key: key);
 
@@ -21,7 +23,7 @@ class _CompleteSupportManagerProfileState
   Widget build(BuildContext context) {
     return const CenteredScrollView(
       children: [
-        CompleteFinancialAdvisorProfileForm(),
+        CompleteSupportManagerProfileForm(),
       ],
     );
   }
